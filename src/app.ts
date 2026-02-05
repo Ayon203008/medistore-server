c3655d3556489795afe83b57a4f493aee96c5db7
Ayon203008 <paypal572874@gmail.com>
2026-02-05 21:17:21 +0600

    prisma import error solved

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
+import { toNodeHandler } from "better-auth/node";
 import express from "express"
+import { auth } from "./lib/auth";
 const app = express()
 
-app.get("/",(req,res)=>{
+app.use(express.json());
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
+
+app.get("/", (req, res) => {
     res.send("Welcome to Medi-Store")
 })
 
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,13 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
-import { Prisma } from "../../generated/prisma/client";
+import { prisma } from "./prisma";
 
 export const auth = betterAuth({
-    database: prismaAdapter(Prisma, {
+    database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
+    emailAndPassword: {
+        enabled: true,
+    },
 });
