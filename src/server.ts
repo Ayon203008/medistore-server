9d22272b83635f214a51808b297e92762460cbef
Ayon203008 <paypal572874@gmail.com>
2026-02-05 20:03:45 +0600

    express problem fixed

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import app from "./app";
+import { prisma } from "./lib/prisma";
+
+const PORT = 5000
+
+async function main() {
+    try {
+        await prisma.$connect()
+        console.log("Connected to the database Successfully")
+        app.listen(PORT,()=>{
+            console.log( `Server is running on port ${PORT}`)
+        })
+
+    } catch (error) {
+        console.log(error)
+        await prisma.$disconnect()
+        process.exit(1)
+    }
+}
+
+main()
